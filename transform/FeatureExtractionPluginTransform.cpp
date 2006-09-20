ed7420bb258ccdbb21a5a5db0f93fd91638f76f7
Chris Cannam <cannam@all-day-breakfast.com>
2006-09-20 15:47:16 +0000

    * Handle plugins with weird block size requirements properly

--- transform/FeatureExtractionPluginTransform.cpp
+++ transform/FeatureExtractionPluginTransform.cpp
@@ -269,8 +269,8 @@ FeatureExtractionPluginTransform::run()
             if (blockFrame >= endFrame) break;
         }
 
-//	std::cerr << "FeatureExtractionPluginTransform::run: blockFrame "
-//		  << blockFrame << std::endl;
+	std::cerr << "FeatureExtractionPluginTransform::run: blockFrame "
+		  << blockFrame << std::endl;
 
 	long completion =
 	    (((blockFrame - startFrame) / m_context.stepSize) * 99) /
