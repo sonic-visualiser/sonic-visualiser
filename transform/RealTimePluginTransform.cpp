8df1bfaead6ccd4ec1ae5209902d443537e5c9e3
Chris Cannam <cannam@all-day-breakfast.com>
2006-11-17 16:27:39 +0000

    * Update copyright notes as appropriate.

--- transform/RealTimePluginTransform.cpp
+++ transform/RealTimePluginTransform.cpp
@@ -1,11 +1,10 @@
-
 /* -*- c-basic-offset: 4 indent-tabs-mode: nil -*-  vi:set ts=8 sts=4 sw=4: */
 
 /*
     Sonic Visualiser
     An audio file viewer and annotation editor.
     Centre for Digital Music, Queen Mary, University of London.
-    This file copyright 2006 Chris Cannam.
+    This file copyright 2006 Chris Cannam and QMUL.
     
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License as
