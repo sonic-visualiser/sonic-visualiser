3537a153d2faae5fc7eaf400cf725baf82fccc98
Chris Cannam <cannam@all-day-breakfast.com>
2007-07-11 17:21:37 +0000

    * Add a colour database, and Add New Colour function to the colour combo   in property box.  The colour property is only correctly handled in the   waveform layer so far.
    * Add en_GB translation, to translate those annoying Color texts in the
      Qt colour picker dialog.

---
+++ i18n/sonic-visualiser_en_GB.ts
@@ -0,0 +1,29 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>QColorDialog</name>
+    <message>
+        <source>Color</source>
+        <translation>Colour</translation>
+    </message>
+    <message>
+        <source>&amp;Basic colors</source>
+        <translation>&amp;Basic colours</translation>
+    </message>
+    <message>
+        <source>&amp;Custom colors</source>
+        <translation>&amp;Custom colours</translation>
+    </message>
+    <message>
+        <source>&amp;Define Custom Colors &gt;&gt;</source>
+        <translation>&amp;Define Custom Colours &gt;&gt;</translation>
+    </message>
+    <message>
+        <source>&amp;Add to Custom Colors</source>
+        <translation>&amp;Add to Custom Colours</translation>
+    </message>
+    <message>
+        <source>Select color</source>
+        <translation>Select colour</translation>
+    </message>
+</context>
+</TS>
