f969a16748ed0bb1a8247151ccd6df8aeaf96282
Chris Cannam <cannam@all-day-breakfast.com>
2008-03-14 17:14:21 +0000

    * More work to abstract out interactive components used in the data library,   so that it does not need to depend on QtGui.

--- main/MainWindow.h
+++ main/MainWindow.h
@@ -29,7 +29,7 @@
 #include "layer/LayerFactory.h"
 #include "transform/Transform.h"
 #include "framework/SVFileReader.h"
-#include "data/fileio/FileFinder.h"
+#include "widgets/FileFinder.h"
 #include "data/fileio/FileSource.h"
 #include "widgets/LayerTreeDialog.h"
 #include <map>
