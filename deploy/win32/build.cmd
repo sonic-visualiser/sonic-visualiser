510f1aab8f960da2b736e429e3184464d9c2aaef
Chris Cannam <cannam@all-day-breakfast.com>
2013-11-05 11:14:16 +0000

    Use non-x86 program files path if x86 version is not found (i.e. on 32-bit Windows) (from with-dependencies branch)

--- deploy/win32/build.cmd
+++ deploy/win32/build.cmd
@@ -1,5 +1,8 @@
 @ECHO OFF
 SET WIXPATH="C:\Program Files (x86)\WiX Toolset v3.7\bin"
+IF NOT EXIST %WIXPATH% (
+    SET WIXPATH="C:\Program Files\WiX Toolset v3.7\bin"
+)
 DEL sonic-visualiser.msi
 %WIXPATH%\candle.exe -v sonic-visualiser.wxs
 %WIXPATH%\light.exe -b ..\.. -ext WixUIExtension -v sonic-visualiser.wixobj
