14c83693592060ceebdc13717b6ef8287c23f6e7
Chris Cannam <cannam@all-day-breakfast.com>
2013-12-05 20:24:40 +0000

    Fix app signing: on os/x 10.9 you need to sign other frameworks etc as well

--- deploy/osx/sign.sh
+++ deploy/osx/sign.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 # Execute this from the top-level directory of the project (the one
 # that contains the .app bundle).  Supply the name of the .app bundle
@@ -11,8 +11,14 @@ if [ -z "$dir" ] || [ ! -d "$dir" ]; then
 	exit 2
 fi
 for app in "$dir"/*.app; do
+    find "$app" -name Qt\* -print | while read fr; do
+	codesign -s "Developer ID Application: Chris Cannam" -fv "$fr"
+    done
+    find "$app" -name \*.dylib -print | while read fr; do
+	codesign -s "Developer ID Application: Chris Cannam" -fv "$fr"
+    done
     codesign -s "Developer ID Application: Chris Cannam" -fv \
-         --requirements '=designated =>  identifier "org.sonicvisualiser.SonicVisualiser" and ( (anchor apple generic and    certificate leaf[field.1.2.840.113635.100.6.1.9] ) or (anchor apple generic and    certificate 1[field.1.2.840.113635.100.6.2.6]  and    certificate leaf[field.1.2.840.113635.100.6.1.13] and    certificate leaf[subject.OU] = "43AQ936H96"))' \
+         --requirements '=designated =>  identifier "org.sonicvisualiser.SonicVisualiser" and ( (anchor apple generic and    certificate leaf[field.1.2.840.113635.100.6.1.9] ) or (anchor apple generic and    certificate 1[field.1.2.840.113635.100.6.2.6]  and    certificate leaf[field.1.2.840.113635.100.6.1.13] and    certificate leaf[subject.OU] = "M2H8666U82"))' \
          "$app"
 done
 
