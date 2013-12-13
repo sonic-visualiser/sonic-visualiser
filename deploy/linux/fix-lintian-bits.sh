b1378ca027b41f1acd12b745251fde58eea5e146
Chris Cannam <c.cannam@qmul.ac.uk>
2013-12-13 09:39:04 +0000

    Add script to fix a few things in dpkg pkg that lintian complains about

---
+++ deploy/linux/fix-lintian-bits.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dir=$1
+
+[ -d "$dir" ] || exit 1
+
+strip "$dir"/usr/bin/*
+
+sz=`du -sx --exclude DEBIAN "$dir" | awk '{ print $1; }'`
+perl -i -p -e "s/Installed-Size: .*/Installed-Size: $sz/" "$dir"/DEBIAN/control
+
+find "$dir" -name \*~ -exec rm \{\} \;
+
+sudo chown -R root.root "$dir"/*
+
