e44e7c4f984ab791b45d8b3fc531baefe7e41ce3
Chris Cannam <cannam@all-day-breakfast.com>
2014-08-14 09:11:36 +0100

    Fix plugin copying (correct target directories)

--- deploy/osx/copy-qt.sh
+++ deploy/osx/copy-qt.sh
@@ -9,9 +9,7 @@ fi
 
 frameworks="QtCore QtNetwork QtGui QtXml QtWidgets QtPrintSupport"
 
-plugins="qtaccessiblewidgets qdds qgif qicns qico qjp2 qjpeg qmng qtga qtiff qwbmp qwebp"
-
-platplugins="qcocoa qminimal"
+plugins="taccessiblewidgets dds gif icns ico jp2 jpeg mng tga tiff wbmp webp cocoa minimal offscreen"
 
 qtdir=$(grep "Command:" Makefile | head -1 | awk '{ print $3; }' | sed s,/bin/.*,,)
 
@@ -22,11 +20,9 @@ fi
 
 fdir="$app.app/Contents/Frameworks"
 pdir="$app.app/Contents/plugins"
-ppdir="$app.app/Contents/plugins/platforms"
 
 mkdir -p "$fdir"
 mkdir -p "$pdir"
-mkdir -p "$ppdir"
 
 echo
 echo "Copying frameworks..."
@@ -39,29 +35,17 @@ echo "Done"
 echo
 echo "Copying plugins..."
 for plug in $plugins; do
-    pfile=$(ls "$qtdir"/plugins/*/lib"$plug".dylib)
+    pfile=$(ls "$qtdir"/plugins/*/libq"$plug".dylib)
     if [ ! -f "$pfile" ]; then
 	echo "Failed to find plugin $plug, exiting"
 	exit 2
     fi
-    cp -v "$pfile" "$pdir" || exit 2
+    target="$pdir"/${pfile##?*plugins/}
+    tdir=`dirname "$target"`
+    mkdir -p "$tdir"
+    cp -v "$pfile" "$target" || exit 2
 done
 
 echo "Done"
 
-echo
-echo "Copying platform plugins..."
-for plug in $platplugins; do
-    pfile=$(ls "$qtdir"/plugins/*/lib"$plug".dylib)
-    if [ ! -f "$pfile" ]; then
-	echo "Failed to find plugin $plug, exiting"
-	exit 2
-    fi
-    # I really cannot be bothered to figure out why Qt fails if I copy
-    # to either one of these alone
-    cp -v "$pfile" "$pdir" || exit 2
-    cp -v "$pfile" "$ppdir" || exit 2
-done
-
-echo "Done"
 
