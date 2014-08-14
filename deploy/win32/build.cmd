d74fc73c15f3a549a95bc9517c91fdc281456954
Chris Cannam <c.cannam@qmul.ac.uk>
2014-08-14 11:45:22 +0100

    Update for Qt 5.3 and WiX 3.8 - transplanted from beta branch

--- deploy/win32/build.cmd
+++ deploy/win32/build.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
-SET WIXPATH="C:\Program Files (x86)\WiX Toolset v3.7\bin"
+SET WIXPATH="C:\Program Files (x86)\WiX Toolset v3.8\bin"
 IF NOT EXIST %WIXPATH% (
-    SET WIXPATH="C:\Program Files\WiX Toolset v3.7\bin"
+    SET WIXPATH="C:\Program Files\WiX Toolset v3.8\bin"
 )
 DEL sonic-visualiser.msi
 %WIXPATH%\candle.exe -v sonic-visualiser.wxs
