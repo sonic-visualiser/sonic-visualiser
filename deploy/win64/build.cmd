e2a4b7430ffeb7f8d56bb18aa0be7cf6e2bcf73d
Chris Cannam <c.cannam@qmul.ac.uk>
2017-01-06 21:38:33 +0000

    Stub win64 deploy, + ogg testfiles

---
+++ deploy/win64/build.cmd
@@ -0,0 +1,11 @@
+@ECHO OFF
+SET WIXPATH="C:\Program Files (x86)\WiX Toolset v3.9\bin"
+IF NOT EXIST %WIXPATH% (
+    SET WIXPATH="C:\Program Files\WiX Toolset v3.9\bin"
+)
+DEL sonic-visualiser.msi
+%WIXPATH%\candle.exe -v sonic-visualiser.wxs
+%WIXPATH%\light.exe -b ..\..\build-sonic-visualiser-Desktop_Qt_5_7_0_MSVC2015_64bit-Release -ext WixUIExtension -v sonic-visualiser.wixobj
+PAUSE
+DEL sonic-visualiser.wixobj
+DEL sonic-visualiser.wixpdb
