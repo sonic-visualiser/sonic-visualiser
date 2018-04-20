dca54461e6a65634298a84f14926ee6c1740408c
Chris Cannam <c.cannam@qmul.ac.uk>
2018-04-20 16:35:48 +0100

    Deployment updates for win32/win64. Next version will be 3.1.

--- deploy/win64/build.cmd
+++ deploy/win64/build.cmd
@@ -1,11 +1,11 @@
 @ECHO OFF
-SET WIXPATH="C:\Program Files (x86)\WiX Toolset v3.9\bin"
+SET WIXPATH="C:\Program Files (x86)\WiX Toolset v3.11\bin"
 IF NOT EXIST %WIXPATH% (
-    SET WIXPATH="C:\Program Files\WiX Toolset v3.9\bin"
+    SET WIXPATH="C:\Program Files\WiX Toolset v3.11\bin"
 )
 DEL sonic-visualiser.msi
 %WIXPATH%\candle.exe -v sonic-visualiser.wxs
-%WIXPATH%\light.exe -b ..\..\..\build-sonic-visualiser-Desktop_Qt_5_7_0_MSVC2015_64bit-Release -ext WixUIExtension -v sonic-visualiser.wixobj
+%WIXPATH%\light.exe -b ..\..\..\build-sonic-visualiser-Desktop_Qt_5_10_1_MSVC2017_64bit-Release -ext WixUIExtension -v sonic-visualiser.wixobj
 PAUSE
 DEL sonic-visualiser.wixobj
 DEL sonic-visualiser.wixpdb
--- version.h
+++ version.h
@@ -1,1 +1,1 @@
-#define SV_VERSION "3.0.4"
+#define SV_VERSION "3.1"
