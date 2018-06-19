94e2e211230a5f6938d9927409714adf1d61b84f
Chris Cannam <c.cannam@qmul.ac.uk>
2018-06-19 15:20:56 +0100

    First cut at updating archive scripts to use Repoint

--- deploy/src/archive.sh
+++ deploy/src/archive.sh
@@ -6,10 +6,21 @@ tag=`hg tags | grep '^sv_v' | head -1 | awk '{ print $1; }'`
 
 v=`echo "$tag" | sed 's/sv_v//' | sed 's/_.*$//'`
 
+current=$(hg id | awk '{ print $1; }')
+
+case "$current" in
+    *+) echo "ERROR: Current working copy has been modified - unmodified copy required so we can update to tag and back again safely"; exit 2;;
+    *);;
+esac
+          
 echo
 echo -n "Packaging up version $v from tag $tag... "
 
-hg archive -r"$tag" --subrepos --exclude sv-dependency-builds /tmp/sonic-visualiser-"$v".tar.gz
+hg update -r"$tag"
+
+./repoint archive /tmp/sonic-visualiser-"$v".tar.gz --exclude sv-dependency-builds repoint.pri
+
+hg update -r"$current"
 
 echo Done
 echo
