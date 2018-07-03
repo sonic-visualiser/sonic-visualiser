0dfc671d944379a51009f0ab71eb53f60a4a5e73
Chris Cannam <cannam@all-day-breakfast.com>
2018-07-03 14:15:51 +0100

    Name things explicitly rather than relying on the other build script

--- deploy/linux/build-and-test-appimage.sh
+++ deploy/linux/build-and-test-appimage.sh
@@ -24,6 +24,18 @@ cat "$dockerdir"/Dockerfile_test_appimage.in | \
     perl -p -e "s/\[\[REVISION\]\]/$current/g" > \
          "$dockerdir"/Dockerfile_test_appimage.gen
 
-"$dockerdir"/build.sh appimage
+dockertag="cannam/sonic-visualiser-appimage-$current"
+
+sudo docker build -t "$dockertag" -f "$dockerdir"/Dockerfile_appimage.gen "$dockerdir"
+
+outdir="$dockerdir/output"
+mkdir -p "$outdir"
+
+container=$(sudo docker create "$dockertag")
+
+sudo docker cp "$container":output.tar "$outdir"
+sudo docker rm "$container"
+
+( cd "$outdir" ; tar xf output.tar && rm -f output.tar )
 
 sudo docker build -f "$dockerdir"/Dockerfile_test_appimage.gen "$dockerdir"
