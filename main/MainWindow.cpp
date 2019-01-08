38c1eec3d435ce7f23e1af48758db04960b24897
Chris Cannam <cannam@all-day-breakfast.com>
2019-01-08 15:23:33 +0000

    Dates and minor text fix

--- main/MainWindow.cpp
+++ main/MainWindow.cpp
@@ -5242,7 +5242,7 @@ MainWindow::about()
     aboutText += "</small></p>";
 
     aboutText += 
-        "<p><small>Sonic Visualiser Copyright &copy; 2005&ndash;2018 Chris Cannam and "
+        "<p><small>Sonic Visualiser Copyright &copy; 2005&ndash;2019 Chris Cannam and "
         "Queen Mary, University of London.</small></p>";
 
     aboutText +=
