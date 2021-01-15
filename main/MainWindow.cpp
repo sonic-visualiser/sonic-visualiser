484527f4e29d91ab44b44945bf3660550f229198
Chris Cannam <cannam@all-day-breakfast.com>
2021-01-15 15:29:06 +0000

    Update copyrights

--- main/MainWindow.cpp
+++ main/MainWindow.cpp
@@ -5521,8 +5521,7 @@ MainWindow::about()
     aboutText += "</small></p>";
 
     aboutText += 
-        "<p><small>Sonic Visualiser Copyright &copy; 2005&ndash;2020 Chris Cannam and "
-        "Queen Mary, University of London.</small></p>";
+        "<p><small>Sonic Visualiser is Copyright &copy; 2005&ndash;2007 Chris Cannam; Copyright &copy; 2006&ndash;2020 Queen Mary, University of London; Copyright &copy; 2020-2021 Particular Programs Ltd.</small></p>";
 
     aboutText +=
         "<p><small>This program is free software; you can redistribute it and/or "
